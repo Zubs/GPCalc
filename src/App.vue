221148cd8efe2f8b9b06aee75638b40f3ee302ef
Zubs <zubairidrisaweda@gmail.com>
2021-02-25 19:01:16 +0100

    Made the page, content soon

--- src/App.vue
+++ src/App.vue
@@ -9,11 +9,29 @@
           </h1>
         </v-card-title>
       </v-app-bar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon class="mr-5" @click="showInfo = true">
+        <v-icon>mdi-info</v-icon>
+      </v-btn>
     </v-app-bar>
 
     <v-main>
       <GPCalc></GPCalc>
     </v-main>
+
+    <v-dialog v-model="showInfo" fullscreen hide-overlay transition="dialog-bottom-transition">
+      <v-card>
+        <v-toolbar dark color="primary">
+          <v-btn icon dark @click="showInfo = false">
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
+          <v-toolbar-title>How We Calculate The GP</v-toolbar-title>
+        </v-toolbar>
+        <v-card-text class="pa-5 ma-5">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit impedit saepe accusantium provident perferendis error, architecto deserunt doloribus, dolore magni? Quas ducimus voluptatem, modi minus excepturi deserunt iusto voluptatum, maiores.</p>
+        </v-card-text>
+      </v-card>
+    </v-dialog>
 
     <v-footer padless app color="primary" dark>
       <v-col class="text-center" cols="12">{{ new Date().getFullYear() }} — <v-btn text rounded dark color="red" href="https://zubs.github.io" target="_blank">Zubair</v-btn></v-col>
@@ -32,7 +50,7 @@ export default {
   },
 
   data: () => ({
-    //
+    showInfo: false,
   }),
 };
 </script>
